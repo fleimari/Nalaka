b74a932501513ad6afa057bc34854c3895a5561a
Janne Heikkinen <t8heja01@students.oamk.fi>
2020-03-26 20:32:15 +0200

    Nimi korjattu

--- app/src/main/java/com/example/nalaka/MainActivity.java
+++ app/src/main/java/com/example/nalaka/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Nimi muutettu
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
