2c1b92c8a4e9712381cce031bd9e78814e493785
Mika <t8hami02@students.oamk.fi>
2020-03-29 21:14:37 +0300

    MainActivitysta siirtyminen + hakun vaihtoehdon muuttamisen testausta

--- app/src/main/java/com/example/nalaka/HakuActivity.java
+++ app/src/main/java/com/example/nalaka/HakuActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.RadioButton;
 
 public class HakuActivity extends AppCompatActivity implements View.OnClickListener {
 
     AutoCompleteTextView atv;
     String[] kaupungit = new String[]{"Helsinki", "Turku", "Tampere", "Oulu", "Vaasa", "Pori", "Rovaniemi", "Espoo", "Vantaa", "Lappeenranta", "Tornio", "Kemi", "Oulunsalo"};
+    String[] ruokalajit = new String[]{"Pizza","Kebab","Porkkanalaatikko","Hapurilainen","Hummus","Keitto","Pihvi","Kalaa"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,4 +32,24 @@ public class HakuActivity extends AppCompatActivity implements View.OnClickListe
         }
 
     }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioKaupunki:
+                if (checked)
+                    atv.setText("");
+                    atv.setAdapter(new ArrayAdapter<String>(HakuActivity.this, android.R.layout.simple_list_item_1, kaupungit));
+                    break;
+            case R.id.radioRuokalaji:
+                if (checked)
+                    atv.setText("");
+                    atv.setAdapter(new ArrayAdapter<String>(HakuActivity.this, android.R.layout.simple_list_item_1, ruokalajit));
+                    break;
+        }
+    }
+
 }
--- app/src/main/java/com/example/nalaka/MainActivity.java
+++ app/src/main/java/com/example/nalaka/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.nalaka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnSiirryHakuun).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnSiirryHakuun)
+        {
+            Intent intentHakuActivity = new Intent(this, HakuActivity.class);
+            startActivity(intentHakuActivity);
+        }
     }
 }
