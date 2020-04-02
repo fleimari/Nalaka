c51657f1d88aa6b3bfca37a3356749a0963e8184
Janne Heikkinen <t8heja01@students.oamk.fi>
2020-04-02 14:48:23 +0300

    XML muokattu

--- app/src/main/java/com/example/nalaka/Paasivu.java
+++ app/src/main/java/com/example/nalaka/Paasivu.java
@@ -2,7 +2,9 @@ package com.example.nalaka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Paasivu extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Paasivu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paasivu);
+    }
+
+    public void goToLuoArvostelu(View view){
+        Intent intent = new Intent(this, luoArvostelu.class);
+        startActivity(intent);
     }
 }
