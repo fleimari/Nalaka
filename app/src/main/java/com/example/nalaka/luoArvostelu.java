068f316f60c92c924b8ee59646f8fd91ab5feb21
Tahkamura <tahkamura@gmail.com>
2020-04-16 14:02:59 +0300

    Spinnereihin tiedot tietokannasta

--- app/src/main/java/com/example/nalaka/luoArvostelu.java
+++ app/src/main/java/com/example/nalaka/luoArvostelu.java
@@ -1,22 +1,33 @@
 package com.example.nalaka;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.media.Image;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class luoArvostelu extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,6 +37,14 @@ public class luoArvostelu extends AppCompatActivity implements View.OnClickListe
     ArrayAdapter<CharSequence> adapterKaupungit, adapterRavintolat, adapterTags;
     private DatabaseReference mDatabase;
     private FirebaseStorage storage; // kuvan liittämiseen jo valmiiksi reference
+    JSONArray arr;
+    ArrayList<String> kaupunkiList;
+    ArrayList<String> ravintolaList;
+    ArrayList<String> tagiList;
+    ArrayAdapter<String> kaupunkiAdapter;
+    ArrayAdapter<String> ravintolaAdapter;
+    ArrayAdapter<String> tagiAdapter;
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +52,24 @@ public class luoArvostelu extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_luo_arvostelu);
 
         mDatabase = FirebaseDatabase.getInstance().getReference(); // Realtime tietokanta reference
+        kaupunkiList = new ArrayList<String>();
+        kaupunkiList.add("");
+        ravintolaList = new ArrayList<String>();
+        ravintolaList.add("");
+        tagiList = new ArrayList<String>();
+        tagiList.add("");
+
+        kaupunkiAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,kaupunkiList);
+        kaupunkiAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ravintolaAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,ravintolaList);
+        ravintolaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        tagiAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,tagiList);
+        tagiAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        queue = Volley.newRequestQueue(this);
+
 
         // Storage tietokanta reference
         //storage = FirebaseStorage.getInstance();
@@ -44,27 +81,29 @@ public class luoArvostelu extends AppCompatActivity implements View.OnClickListe
          spinnerRavintola = (Spinner) findViewById(R.id.spinnerRavintola);
          spinnerTags = (Spinner) findViewById(R.id.spinnerTags);
 
-        adapterKaupungit = ArrayAdapter.createFromResource(this, R.array.kaupungit, android.R.layout.simple_spinner_item);
-        adapterKaupungit.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerKaupunki.setAdapter(adapterKaupungit);
-
-        adapterRavintolat = ArrayAdapter.createFromResource(this,  R.array.ravintolat, android.R.layout.simple_spinner_item);
-        adapterRavintolat.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerRavintola.setAdapter(adapterRavintolat);
-
-        adapterTags = ArrayAdapter.createFromResource(this,  R.array.tags, android.R.layout.simple_spinner_item);
-        adapterTags.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerTags.setAdapter(adapterTags);
+         spinnerKaupunki.setAdapter(kaupunkiAdapter);
+         spinnerRavintola.setAdapter(ravintolaAdapter);
+         spinnerTags.setAdapter(tagiAdapter);
 
         findViewById(R.id.button2).setOnClickListener(this);
+        haeKaupungit();
+        haeTagit();
 
+        spinnerKaupunki.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                Object item = parent.getItemAtPosition(pos);
+                haeRavintolat(item.toString());
+            }
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
 
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.button2){
-            lisaaArvostelu();
+            //lisaaArvostelu();
         }
 
     }
@@ -124,24 +163,91 @@ public class luoArvostelu extends AppCompatActivity implements View.OnClickListe
         mDatabase.child("Arvostelut").child(var).child("Tags").push().setValue(tag2);
     }
 
-    public void lisaaTagi(){
-
-        // Yksi rivi lisää aina yhden tagin Tags-tauluun
-        mDatabase.child("Tags").push().setValue("Piksa");
-        //mDatabase.child("Tags").push().setValue("Kepapo");
-        //mDatabase.child("Tags").push().setValue("Italialainen");
-        //mDatabase.child("Tags").push().setValue("Grillimättö");
-        //mDatabase.child("Tags").push().setValue("Kiinalainen");
+    public void lisaaTagi(String tagi){
+        mDatabase.child("Tags").push().setValue(tagi);
     }
 
-    public void lisaaKaupunki(){
-
-        String kaupunki = "Muhos";
-        String ravintola = "Muhoksen piksaKepapo";
+    public void lisaaKaupunki(String kaupunki, String ravintola){
 
         mDatabase.child("Kaupungit").child(kaupunki).push().setValue(ravintola);
-
     }
 
+    public void haeKaupungit(){
 
+        String url = "https://eighth-anvil-272013.firebaseio.com/Kaupungit.json?print=pretty";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray list = response.names();
+                            for(int i = 0; i<response.length();i++){
+                                kaupunkiList.add(list.get(i).toString());
+                                kaupunkiAdapter.notifyDataSetChanged();
+                            }
+                        }catch (Exception e){}
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+        queue.add(jsonObjectRequest);
+    }
+
+    public void haeRavintolat(final String kaupunki){
+
+        ravintolaList.clear();
+        ravintolaList.add("");
+        ravintolaAdapter.notifyDataSetChanged();
+        String url = "https://eighth-anvil-272013.firebaseio.com/Kaupungit.json?print=pretty";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try{
+                            JSONObject arr = response.getJSONObject(kaupunki);
+                            JSONArray testlist = arr.names();
+                            for(int i =0;i<testlist.length();i++){
+                                String testi = arr.getString(testlist.getString(i).toString());
+                                ravintolaList.add(testi);
+                                kaupunkiAdapter.notifyDataSetChanged();
+                            }
+
+                        }catch (Exception e){}
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+    }
+
+    public void haeTagit(){
+        String url = "https://eighth-anvil-272013.firebaseio.com/Tags.json?print=pretty";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            //JSONObject arr = response.getJSONObject("Tags");
+                            JSONArray testlist = response.names();
+                            for(int i =0;i<testlist.length();i++){
+                                String testi = response.getString(testlist.getString(i).toString());
+                                tagiList.add(testi);
+                                tagiAdapter.notifyDataSetChanged();
+                            }
+                        }catch (Exception e){}
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
+
+        queue.add(jsonObjectRequest);
+    }
 }
