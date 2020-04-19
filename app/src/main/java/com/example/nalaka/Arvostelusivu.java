e16a1a63092d4456d03abbb55f96161d6c32452d
Janne Heikkinen <t8heja01@students.oamk.fi>
2020-04-19 22:35:55 +0300

    Näkyvyyskokeiluja

--- app/src/main/java/com/example/nalaka/Arvostelusivu.java
+++ app/src/main/java/com/example/nalaka/Arvostelusivu.java
@@ -20,6 +20,7 @@ public class Arvostelusivu extends AppCompatActivity {
 
     VideoView videoPlayer;
     ImageView imageViewer;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +34,16 @@ public class Arvostelusivu extends AppCompatActivity {
         String videoUrl = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4";
         //String videoUrl = "https://firebasestorage.googleapis.com/v0/b/eighth-anvil-272013.appspot.com/o/testi.mp4?alt=media&token=c8e085b3-30ff-4c75-8dd2-cb33e2ea5eb1";
 
-
         Uri viUri = Uri.parse(videoUrl);
         videoPlayer.setVideoURI(viUri);
 
         MediaController mediaController = new MediaController(this);
         videoPlayer.setMediaController(mediaController);
         mediaController.setAnchorView(videoPlayer);
+
+        new DownloadImageTask(imageViewer)
+                .execute("https://www.worldatlas.com/r/w728-h425-c728x425/upload/06/06/04/shutterstock-591122330.jpg");
+        //.execute("https://firebasestorage.googleapis.com/v0/b/eighth-anvil-272013.appspot.com/o/It%27s%20me.png?alt=media&token=bb1db93d-2007-4bcc-8fe0-66b8f4d271c0");
 
     }
 
@@ -73,6 +77,22 @@ public class Arvostelusivu extends AppCompatActivity {
         protected void onPostExecute(Bitmap result) {
             bmImage.setImageBitmap(result);
         }
+    }
+
+    public void changeView(View v) {
+
+        i++;
+        if (i == 1) {
+            videoPlayer.setVisibility(View.VISIBLE);
+            imageViewer.setVisibility(View.INVISIBLE);
+        }
+
+        if (i == 2) {
+            i = 0;
+            videoPlayer.setVisibility(View.INVISIBLE);
+            imageViewer.setVisibility(View.VISIBLE);
+        }
+
     }
 
 
