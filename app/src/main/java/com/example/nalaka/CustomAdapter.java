b3551ecbe37549f4dcda81dc713ee7bba28745f3
Mika <t8hami02@students.oamk.fi>
2020-04-20 15:36:28 +0300

    Kuvan lisäämisen testaustausta listviewn elemettiin!

--- app/src/main/java/com/example/nalaka/CustomAdapter.java
+++ app/src/main/java/com/example/nalaka/CustomAdapter.java
@@ -2,6 +2,10 @@ package com.example.nalaka;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +15,7 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -61,15 +66,43 @@ public class CustomAdapter extends ArrayAdapter<ArvosteluClass> {
 
         TextView text = (TextView) vi.findViewById(R.id.arvosteluteksti);
         TextView title = vi.findViewById(R.id.title);
-        ImageView image = vi.findViewById(R.id.listIMG);
+        ImageView image = (ImageView) vi.findViewById(R.id.listIMG);
 
         RatingBar stars = (RatingBar) vi.findViewById(R.id.starRating);
         stars.setRating(Float.parseFloat(currentArvostelu.getPisteet()));
 
         text.setText(currentArvostelu.getArvosteluTeksti());
         title.setText(currentArvostelu.getOtsikko());
+
+
+        Log.d("testi2", "getView: " + currentArvostelu.getKuvaUrl());
+        new DownloadImageTask(image).execute(currentArvostelu.getKuvaUrl());
         //image.setImageURI(currentArvostelu.getKuvaUrl());
+
 
         return vi;
     }
+    class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urlImage = urls[0];
+            Bitmap picImage = null;
+            try {
+                InputStream in = new java.net.URL(urlImage).openStream();
+                picImage = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error to load image", e.getMessage());
+                e.printStackTrace();
+            }
+            return picImage;
+        }
+    }
+
+
 }
+
