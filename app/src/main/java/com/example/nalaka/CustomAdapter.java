6457896a277918b9617e918a576158dc2839fb8d
Mika <t8hami02@students.oamk.fi>
2020-04-21 13:34:19 +0300

    Listview lisätty ravintola ja tagit

--- app/src/main/java/com/example/nalaka/CustomAdapter.java
+++ app/src/main/java/com/example/nalaka/CustomAdapter.java
@@ -1,16 +1,12 @@
 package com.example.nalaka;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -20,6 +16,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
 
+import org.w3c.dom.Text;
+
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,9 +26,6 @@ public class CustomAdapter extends ArrayAdapter<ArvosteluClass> {
 
     private  Context context;
     private ArrayList<ArvosteluClass> arvostelutList = new ArrayList<>();
-    //private final String[] data;
-    //private final String[] otsikkoData;
-    //private final Integer[] imageIDarray;
     private static LayoutInflater inflater = null;
 
     public CustomAdapter(Context context, ArrayList<ArvosteluClass> list){
@@ -39,22 +34,6 @@ public class CustomAdapter extends ArrayAdapter<ArvosteluClass> {
         this.arvostelutList = list;
     }
 
-    /*
-    @Override
-    public int getCount() {
-        return data.length;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return data[position];
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }*/
-
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -62,15 +41,13 @@ public class CustomAdapter extends ArrayAdapter<ArvosteluClass> {
 
         if(vi == null)
             vi = LayoutInflater.from(context).inflate(R.layout.list_row,parent,false);
-            //vi = inflater.inflate(R.layout.list_row, null);
 
         ArvosteluClass currentArvostelu = arvostelutList.get(position);
 
-
-
-
         TextView text = (TextView) vi.findViewById(R.id.arvosteluteksti);
         TextView title = vi.findViewById(R.id.title);
+        TextView ravintola = vi.findViewById(R.id.textViewListRavintola);
+        TextView tagit = vi.findViewById(R.id.textViewListTagit);
         final ImageView image = (ImageView) vi.findViewById(R.id.listIMG);
         VideoView video = (VideoView) vi.findViewById(R.id.listVideo);
 
@@ -79,7 +56,13 @@ public class CustomAdapter extends ArrayAdapter<ArvosteluClass> {
 
         text.setText(currentArvostelu.getArvosteluTeksti());
         title.setText(currentArvostelu.getOtsikko());
+        ravintola.setText("Ravintola: " + currentArvostelu.getRavintola());
+        tagit.setText("Tagit:");
 
+        for (int i = 0; i < currentArvostelu.getTagit().size(); i ++)
+        {
+            tagit.append(" " + currentArvostelu.getTagit().get(i));
+        }
 
 
         Log.d("Testi2", "getView: " + currentArvostelu.getKuvaUrl());
@@ -89,12 +72,17 @@ public class CustomAdapter extends ArrayAdapter<ArvosteluClass> {
             if (currentArvostelu.getViedoUrl().isEmpty())
             {
                 image.setImageResource(R.drawable.pizzaimg);
-                vi.findViewById(R.id.listVideo);
+                //image.setVisibility(View.INVISIBLE);
+                //video.setVisibility(View.VISIBLE);
             }
-
+            image.setImageResource(R.drawable.pizzaimg);
+            //Uri videoUri = Uri.parse(currentArvostelu.getViedoUrl());
+            //video.setVideoURI(videoUri);
         }
         else
         {
+            //image.setVisibility(View.VISIBLE);
+            //video.setVisibility(View.INVISIBLE);
             ImageRequest imageRequest = new ImageRequest(currentArvostelu.getKuvaUrl(),
                     new Response.Listener<Bitmap>() {
                         @Override
@@ -109,7 +97,6 @@ public class CustomAdapter extends ArrayAdapter<ArvosteluClass> {
                     });
             MySingleton.getInstance().addToRequestQueue(imageRequest);
         }
-
 
         return vi;
     }
